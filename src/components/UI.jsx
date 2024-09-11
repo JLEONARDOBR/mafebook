6aade95051f3a0cad9d3e1ce66d45c2c31d77a71
SalsaFilmes <j.leonardo.cn@hotmail.com>
2024-09-11 16:25:45 -0300

    q

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -89,54 +89,54 @@ export const UI = () => {
         <div className="relative">
           <div className="bg-white/0  animate-horizontal-scroll flex items-center gap-8 w-max px-8">
             <h1 className="shrink-0 text-white text-10xl font-black ">
-              Wawa Sensei
+            Produção de Conteúdo Audiovisual
             </h1>
             <h2 className="shrink-0 text-white text-8xl italic font-light">
-              React Three Fiber
+            Social Media
             </h2>
             <h2 className="shrink-0 text-white text-12xl font-bold">
-              Three.js
+            Produção de Conteúdo Audiovisual
             </h2>
             <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Ultimate Guide
+            Social Media
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-medium">
-              Tutorials
+            Produção de Conteúdo Audiovisual
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              Learn
+            Social Media
             </h2>
             <h2 className="shrink-0 text-white text-13xl font-bold">
-              Practice
+            Produção de Conteúdo Audiovisual
             </h2>
             <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-              Creative
+            Social Media
             </h2>
           </div>
           <div className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max">
             <h1 className="shrink-0 text-white text-10xl font-black ">
-              Wawa Sensei
+            Produção de Conteúdo Audiovisual
             </h1>
             <h2 className="shrink-0 text-white text-8xl italic font-light">
-              React Three Fiber
+            Social Media
             </h2>
             <h2 className="shrink-0 text-white text-12xl font-bold">
-              Three.js
+            Produção de Conteúdo Audiovisual
             </h2>
             <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Ultimate Guide
+            Social Media
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-medium">
-              Tutorials
+            Produção de Conteúdo Audiovisual
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              Learn
+            Social Media
             </h2>
             <h2 className="shrink-0 text-white text-13xl font-bold">
-              Practice
+            Produção de Conteúdo Audiovisual
             </h2>
             <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-              Creative
+            Social Media
             </h2>
           </div>
         </div>
