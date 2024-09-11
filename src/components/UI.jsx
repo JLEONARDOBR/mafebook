a74ddee3fae40727a6b93e8dc73b3d8fdd6ee3ef
SalsaFilmes <j.leonardo.cn@hotmail.com>
2024-09-11 19:03:44 -0300

    a

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -49,19 +49,28 @@ export const UI = () => {
 
   return (
     <>
-      <main className=" pointer-events-none select-none z-10 fixed  inset-0  flex justify-between flex-col">
+      <main className="pointer-events-none select-none z-10 fixed inset-0 flex justify-between flex-col">
         <a
           className="pointer-events-auto mt-10 ml-10"
           href="https://mafe-teste.netlify.app/"
         >
           <img className="w-20" src="/images/wawasensei-white.png" />
         </a>
+
+        {/* Novo botão no canto superior direito */}
+        <a
+          className="pointer-events-auto mt-10 mr-10 absolute top-0 right-0"
+          href="https://mafe-teste.netlify.app/"
+        >
+          <img className="w-20" src="/images/wawasensei-white.png" />
+        </a>
+
         <div className="w-full overflow-auto pointer-events-auto flex justify-center">
           <div className="overflow-auto flex items-center gap-4 max-w-full p-10">
             {[...pages].map((_, index) => (
               <button
                 key={index}
-                className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${
+                className={`border-transparent hover:border-white transition-all duration-300 px-4 py-3 rounded-full text-lg uppercase shrink-0 border ${
                   index === page
                     ? "bg-white/90 text-black"
                     : "bg-black/30 text-white"
@@ -72,7 +81,7 @@ export const UI = () => {
               </button>
             ))}
             <button
-              className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${
+              className={`border-transparent hover:border-white transition-all duration-300 px-4 py-3 rounded-full text-lg uppercase shrink-0 border ${
                 page === pages.length
                   ? "bg-white/90 text-black"
                   : "bg-black/30 text-white"
@@ -86,64 +95,64 @@ export const UI = () => {
       </main>
 
       <div className="fixed inset-0 flex items-center -rotate-2 select-none">
-  <div className="relative">
-    <div
-      className="bg-white/0 animate-horizontal-scroll flex items-center gap-8 w-max px-8"
-      style={{ animationDuration: '60s' }} // Aplica aqui
-    >
-      <h1 className="shrink-0 text-white text-10xl font-black">
-        Produção de Conteúdo Audiovisual
-      </h1>
-      <h2 className="shrink-0 text-white text-8xl italic font-light">
-        Social Media
-      </h2>
-      <h2 className="shrink-0 text-white text-12xl font-bold">
-        Produção de Conteúdo Audiovisual
-      </h2>
-      <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-        Social Media
-      </h2>
-      <h2 className="shrink-0 text-white text-9xl font-medium">
-        Produção de Conteúdo Audiovisual
-      </h2>
-      <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-        Social Media
-      </h2>
-      <h2 className="shrink-0 text-white text-13xl font-bold">
-        Produção de Conteúdo Audiovisual
-      </h2>
-      <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-        Social Media
-      </h2>
-    </div>
-    <div
-      className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max"
-      style={{ animationDuration: '60s' }} // Aplica aqui
-    >
-      <h1 className="shrink-0 text-white text-10xl font-black">
-        Produção de Conteúdo Audiovisual
-      </h1>
-      <h2 className="shrink-0 text-white text-8xl italic font-light">
-        Social Media
-      </h2>
-      <h2 className="shrink-0 text-white text-12xl font-bold">
-        Produção de Conteúdo Audiovisual
-      </h2>
-      <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-        Social Media
-      </h2>
-      <h2 className="shrink-0 text-white text-9xl font-medium">
-        Produção de Conteúdo Audiovisual
-      </h2>
-      <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-        Social Media
-      </h2>
-      <h2 className="shrink-0 text-white text-13xl font-bold">
-        Produção de Conteúdo Audiovisual
-      </h2>
-      <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-        Social Media
-      </h2>
+        <div className="relative">
+          <div
+            className="bg-white/0 animate-horizontal-scroll flex items-center gap-8 w-max px-8"
+            style={{ animationDuration: '60s' }}
+          >
+            <h1 className="shrink-0 text-white text-10xl font-black">
+              Produção de Conteúdo Audiovisual
+            </h1>
+            <h2 className="shrink-0 text-white text-8xl italic font-light">
+              Social Media
+            </h2>
+            <h2 className="shrink-0 text-white text-12xl font-bold">
+              Produção de Conteúdo Audiovisual
+            </h2>
+            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
+              Social Media
+            </h2>
+            <h2 className="shrink-0 text-white text-9xl font-medium">
+              Produção de Conteúdo Audiovisual
+            </h2>
+            <h2 className="shrink-0 text-white text-9xl font-extralight italic">
+              Social Media
+            </h2>
+            <h2 className="shrink-0 text-white text-13xl font-bold">
+              Produção de Conteúdo Audiovisual
+            </h2>
+            <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
+              Social Media
+            </h2>
+          </div>
+          <div
+            className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max"
+            style={{ animationDuration: '60s' }}
+          >
+            <h1 className="shrink-0 text-white text-10xl font-black">
+              Produção de Conteúdo Audiovisual
+            </h1>
+            <h2 className="shrink-0 text-white text-8xl italic font-light">
+              Social Media
+            </h2>
+            <h2 className="shrink-0 text-white text-12xl font-bold">
+              Produção de Conteúdo Audiovisual
+            </h2>
+            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
+              Social Media
+            </h2>
+            <h2 className="shrink-0 text-white text-9xl font-medium">
+              Produção de Conteúdo Audiovisual
+            </h2>
+            <h2 className="shrink-0 text-white text-9xl font-extralight italic">
+              Social Media
+            </h2>
+            <h2 className="shrink-0 text-white text-13xl font-bold">
+              Produção de Conteúdo Audiovisual
+            </h2>
+            <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
+              Social Media
+            </h2>
           </div>
         </div>
       </div>
