0274f9a75c62e137fb821560360966bd8bdf8f8b
SalsaFilmes <j.leonardo.cn@hotmail.com>
2024-09-11 18:19:33 -0300

    a

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,39 +1,20 @@
 import { Environment, Float, OrbitControls } from "@react-three/drei";
-import { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
-import { Euler } from "three";
 import { Book } from "./Book";
 
 export const Experience = () => {
-  const controlsRef = useRef();
-
-  // Limita a rotação nos eixos X, Y e Z para no máximo 10 graus (~0.1745 radianos)
-  useFrame(() => {
-    if (controlsRef.current) {
-      const maxRotation = 0.1745; // 10 degrees in radians
-      const minRotation = -0.1745;
-
-      const { x, y, z } = controlsRef.current.getAzimuthalAngle();
-      controlsRef.current.object.rotation.x = Math.max(minRotation, Math.min(maxRotation, x));
-      controlsRef.current.object.rotation.y = Math.max(minRotation, Math.min(maxRotation, y));
-      controlsRef.current.object.rotation.z = Math.max(minRotation, Math.min(maxRotation, z));
-    }
-  });
-
   return (
     <>
       <Float
         rotation-x={-Math.PI / 4}
-        floatIntensity={1}
-        speed={2}
-        rotationIntensity={2}
+        floatIntensity={0.5}
+        speed={0.5}
+        rotationIntensity={0.5}
       >
         <Book />
       </Float>
+      {/* OrbitControls com rotação nos eixos Y e Z desativados */}
       <OrbitControls
-        ref={controlsRef}
-        maxPolarAngle={Math.PI / 2 + 0.1745} // Limita rotação vertical no eixo X
-        minPolarAngle={Math.PI / 2 - 0.1745} // Limita rotação vertical no eixo X
+        enableRotate={false} // Desativa rotação nos eixos X, Y, Z
       />
       <Environment preset="studio" />
       <directionalLight
